c94f18f4485d87341c90b1f2269a9ea66b03dc48
Axel Kalland <axelkalland@gmail.com>
2019-05-28 11:51:09 +0200

    starting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Skill.destroy_all
+DevelopSkill.destroy_all
+User.destroy_all
+Vacancy.destroy_all
+VacancySkill.destroy_all
+Message.destroy_all
+Conversation.destroy_all
+
+puts "Creating shit"
