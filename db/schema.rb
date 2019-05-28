34521c6377ac617676c816e2667b847aa35cd2ac
Alexander Miesen <alexmiesen@hotmail.com>
2019-05-28 11:35:59 +0200

    added location to user and vacancy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_163331) do
+ActiveRecord::Schema.define(version: 2019_05_28_092049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_163331) do
     t.string "description"
     t.string "department"
     t.boolean "is_manager", default: false
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -89,6 +90,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_163331) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
     t.index ["user_id"], name: "index_vacancies_on_user_id"
   end
 
