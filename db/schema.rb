f4c4548dd65bfc304c68747f286f8a58b8aa42a6
Alexander Miesen <alexmiesen@hotmail.com>
2019-05-28 11:40:07 +0200

    added photo to vacancies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_092049) do
+ActiveRecord::Schema.define(version: 2019_05_28_093743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_092049) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "location"
+    t.string "photo"
     t.index ["user_id"], name: "index_vacancies_on_user_id"
   end
 
