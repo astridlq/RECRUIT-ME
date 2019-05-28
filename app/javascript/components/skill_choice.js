3dda51b6f192b63a8d5bf144b801af4acd4760a9
Astrid Le Quere <astrid.lequere@gmail.com>
2019-05-28 15:11:27 +0200

    skill front-end done

---
+++ app/javascript/components/skill_choice.js
@@ -0,0 +1,5 @@
+
+const btn = document.querySelector('.skill-choice')
+btn.addEventListener("click", (event) => {
+  btn.classList.toggle("active");
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,4 @@
 import "bootstrap";
+import { skillActive } from '../components/skill-choice.js';
+
+skillActive();
