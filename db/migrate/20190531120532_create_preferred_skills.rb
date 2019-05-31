59cb710ac90bba7b4b56ced5c2ffb3174ecff057
Astrid Le Quere <astrid.lequere@gmail.com>
2019-05-31 14:26:54 +0200

    small fixes nav + model

--- db/migrate/20190531120532_create_preferred_skills.rb
+++ db/migrate/20190531120532_create_preferred_skills.rb
@@ -1,8 +1,8 @@
 class CreatePreferredSkills < ActiveRecord::Migration[5.2]
   def change
     create_table :preferred_skills do |t|
-      t.references :vacancy_id, foreign_key: true
-      t.references :skill_id, foreign_key: true
+      t.references :vacancy, foreign_key: true
+      t.references :skill, foreign_key: true
 
       t.timestamps
     end
