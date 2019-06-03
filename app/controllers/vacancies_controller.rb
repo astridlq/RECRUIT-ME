4e70528fe547ff7f24af1e95596d8195d9b3d941
Astrid Le Quere <astrid.lequere@gmail.com>
2019-06-03 09:44:32 +0200

    show method in the controller ok

--- app/controllers/vacancies_controller.rb
+++ app/controllers/vacancies_controller.rb
@@ -1,9 +1,20 @@
 class VacanciesController < ApplicationController
+  before_action :set_vacancies, only: [:show]
+
   def index
     @vacancies = Vacancy.all
     @vacancies = policy_scope(Vacancy).order(created_at: :desc)
   end
 
   def show
+    @vacancy_skills = @vacancy.vacancy_skills
+    @preferred_skills = @vacancy.preferred_skills
+  end
+
+  private
+
+  def set_vacancies
+    @vacancy = Vacancy.find(params[:id])
+    authorize @vacancy
   end
 end
