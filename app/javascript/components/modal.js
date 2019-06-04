72f9ecb7ad441c70f5c862d3b8a838c72821a881
Edward Phillips <edphil@hotmail.co.uk>
2019-06-04 22:01:43 +0200

    added little fixes

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -1,36 +1,19 @@
-// Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// Get the submit element that closes the modal
-var submit = document.getElementsByClassName("modal-submit")[0];
+var cancel = document.querySelector(".modal-cancel");
+var submit = document.querySelector(".modal-submit");
+var messageField = document.getElementById("conversation_content");
 
 const modalOpen = () => {
-  // When the user clicks on the button, open the modal
   btn.onclick = function() {
+    messageField.value = ""
     modal.style.display = "block";
   }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
+  cancel.onclick = function() {
     modal.style.display = "none";
   }
-
-  // When the user submits, close the modal
   submit.onclick = function() {
     modal.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
   }
 }
 
